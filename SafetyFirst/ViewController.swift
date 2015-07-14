0c74e279748038adf9ba47e3b487691c58b7c787
Vincent Pacul <Vincent@Fais-iMac.local>
2015-07-14 11:32:20 +0800

    Initial Commit

---
+++ SafetyFirst/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SafetyFirst
+//
+//  Created by Vincent Pacul on 14/7/15.
+//  Copyright (c) 2015 Vincent Pacul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
